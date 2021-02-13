5043d4246645e67541b0dd4e6698f15f2494e49c
koushith <koushith97@gmail.com>
2021-02-14 01:06:58 +0530

    adds basic layout

---
+++ components/Overview.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Overview() {
+  return (
+    <div style={{ height: '70vh' }}>
+      <h1>hello</h1>
+      <h2>Hello</h2>
+    </div>
+  );
+}
+
+export default Overview;
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,14 @@
 import Layout from '../components/Layout';
-import Dashboard from '../components/Dashboard'
+import Overview from '../components/Overview';
 import { JssProvider } from 'react-jss';
 const Home = (props) => {
+  return (
+    <JssProvider id={{ minify: true }}>
+      <Layout toggleDarkMode={props.toggleDarkMode}>
+        <Overview />
+      </Layout>
+    </JssProvider>
+  );
+};
 
-  
-    return (
-        <JssProvider id={{ minify: true }}>
-        <Layout toggleDarkMode={props.toggleDarkMode}>
-            <Dashboard/>
-        </Layout>
-        </JssProvider>
-    )
-}
-    
-    
-  
-  export default Home;
+export default Home;
