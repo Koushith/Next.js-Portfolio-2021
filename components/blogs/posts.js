c1bab44c72366001d267d5a8d83ee4cb69d3d248
koushith <koushith97@gmail.com>
2021-02-27 03:03:14 +0530

    adds ghost cms

---
+++ components/blogs/posts.js
@@ -0,0 +1,10 @@
+import GhostContentAPI from '@tryghost/content-api';
+
+// Create API instance with site credentials
+// const B
+// const API_KEYS = 'edb46fdd278f8877272395426d';
+const api = new GhostContentAPI({
+  url: 'https://demo.ghost.io',
+  key: '22444f78447824223cefc48062',
+  version: 'v3',
+});
