167ceeb33d62c5f2ab13e2acbb18f64908f2d83a
koushith <koushith97@gmail.com>
2021-03-24 17:21:02 +0530

    adds google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,24 @@
+import { useEffect } from 'react';
+
 import '../css/index.css';
 import Head from 'next/head';
 import Layout from '@components/Layout';
 import { ThemeProvider } from 'next-themes';
+import { useRouter } from 'next/router';
+import * as gtag from '../lib/gtag';
 //import Container from "@components/Container";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
   return (
     <ThemeProvider attribute='class'>
       <Layout>
