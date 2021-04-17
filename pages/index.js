0e80e181097ed6223425280daf292cf9e3709226
koushith <koushith97@gmail.com>
2021-04-17 22:17:38 +0530

    fixes broken links

--- pages/index.js
+++ pages/index.js
@@ -80,47 +80,10 @@ export default function Home() {
       <ProjectCard
         title='Documentation Series'
         description='Documenting my Learning process along the way. This helps me to track my progress over time. '
-        href='https://koushith97.medium.com/first-blog-post-testing-8999201aeddd'
+        href='https://www.youtube.com/channel/UC2jU6KNhrAPJ4LIWDuiagnA'
         icon='nextjs'
       />
-      {/* <BlogPost
-          title='Everything I Know About Style Guides, Design Systems, and Component Libraries'
-          summary="A deep-dive on everything I've learned in the past year building style guides, design systems, component libraries, and their best practices."
-          slug='style-guides-component-libraries-design-systems'
-        />
-        <BlogPost
-          title='How Stripe Designs Beautiful Websites'
-          summary="Examining the tips and tricks used to make Stripe's website design a notch above the rest."
-          slug='how-stripe-designs-beautiful-websites'
-        />
-        <BlogPost
-          title='Creating a Monorepo with Lerna & Yarn Workspaces'
-          summary='In this guide, you will learn how to create a Monorepo to manage multiple packages with a shared build, test, and release process.'
-          slug='monorepo-lerna-yarn-workspaces'
-        />
-        <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white'>
-          Projects
-        </h3>
-        <ProjectCard
-          title='React 2025'
-          description='Build and deploy a modern Jamstack application using the most popular open-source software.'
-          href='https://react2025.com/'
-          icon='react2025'
-        />
-        <ProjectCard
-          title='Learn Next.js'
-          description='A free video course for building static and server-side rendered applications with Next.js and React.'
-          href='https://masteringnextjs.com/'
-          icon='nextjs'
-        />
-        <ProjectCard
-          title='Fast Feedback'
-          description='The easiest way to add comments or reviews to your static site. Built as part of React 2025.'
-          href='https://fastfeedback.io/'
-          icon='fastfeedback'
-        />
-        <Timeline />
-        <Subscribe /> */}
+
       <Timeline />
     </div>
   );
