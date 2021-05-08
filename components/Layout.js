94758e2ee1d5e7232b22cdc4b0bf62e571e69df8
koushith <koushith97@gmail.com>
2021-05-08 17:59:32 +0530

    fixes deployment errors

--- components/Layout.js
+++ components/Layout.js
@@ -2,9 +2,9 @@ import Head from 'next/head';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
-import Footer from '@components/Footer';
-import Link from 'next/link';
-import Header from '@components/Header';
+import Footer from './Footer';
+
+import Header from './Header';
 
 // import Footer from '@/components/Footer';
 
