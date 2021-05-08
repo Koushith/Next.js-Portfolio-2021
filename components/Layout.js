2416fb01bd6a3683d991ac60f383c745cf5e987b
koushith <koushith97@gmail.com>
2021-05-08 17:46:39 +0530

    fixes deployment errors

--- components/Layout.js
+++ components/Layout.js
@@ -4,7 +4,7 @@ import { useState, useEffect } from 'react';
 import { useTheme } from 'next-themes';
 import Footer from '@components/Footer';
 import Link from 'next/link';
-import Header from './Header';
+import Header from '@components/Header';
 
 // import Footer from '@/components/Footer';
 
@@ -46,52 +46,6 @@ export default function Container(props) {
           <meta property='article:published_time' content={meta.date} />
         )}
       </Head>
-      {/* <nav className='sticky-nav flex justify-between items-center max-w-4xl w-full p-8 my-0 md:my-8 mx-auto bg-white dark:bg-black bg-opacity-60'>
-        <div>
-          <Link href='/'>
-            <a className='p-1 sm:p-4 text-gray-900 dark:text-gray-100'>Home</a>
-          </Link>
-          <Link href='/about'>
-            <a className='p-1 sm:p-4 text-gray-900 dark:text-gray-100'>About</a>
-          </Link>
-          <Link href='/blog'>
-            <a className='p-1 sm:p-4 text-gray-900 dark:text-gray-100'>Blog</a>
-          </Link>
-        </div>
-
-        <button
-          aria-label='Toggle Dark Mode'
-          type='button'
-          className='bg-gray-200 dark:bg-gray-800 rounded p-3 h-10 w-10'
-          onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}
-        >
-          {mounted && (
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              fill='currentColor'
-              stroke='currentColor'
-              className='h-4 w-4 text-gray-800 dark:text-gray-200'
-            >
-              {theme === 'dark' ? (
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M12 3v1m0 16v1m9-9h-1M4 12H3m15.364 6.364l-.707-.707M6.343 6.343l-.707-.707m12.728 0l-.707.707M6.343 17.657l-.707.707M16 12a4 4 0 11-8 0 4 4 0 018 0z'
-                />
-              ) : (
-                <path
-                  strokeLinecap='round'
-                  strokeLinejoin='round'
-                  strokeWidth={2}
-                  d='M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z'
-                />
-              )}
-            </svg>
-          )}
-        </button>
-      </nav> */}
 
       <Header
         mounted={mounted}
