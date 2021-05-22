3c17c08d77c47ea66c79b9138761337645e591f2
koushith <koushith97@gmail.com>
2021-05-22 16:20:34 +0530

    skills-wip

---
+++ components/Skills.js
@@ -0,0 +1,120 @@
+export const Skills = () => {
+  return (
+    <div className='px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-12'>
+      <div className='max-w-xl mb-10 md:mx-auto sm:text-center lg:max-w-2xl md:mb-12'>
+        <h2 className='max-w-lg mb-6 font-sans text-lg font-bold leading-none tracking-tight text-gray-900 sm:text-4xl md:mx-auto dark:text-gray-100'>
+          What do I know?
+        </h2>
+        <p className='prose text-gray-600 dark:text-gray-400 mb-6'>
+          Exploring the tech stack infinities and Blockchain.
+        </p>
+      </div>
+      <div className='grid grid-cols-2 gap-5 row-gap-6 mb-10 sm:grid-cols-3 lg:grid-cols-5'>
+        <div className='text-center'>
+          <div className='flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-24 sm:h-24'>
+            <svg
+              className='w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16'
+              stroke='currentColor'
+              viewBox='0 0 52 52'
+            >
+              <polygon
+                strokeWidth='3'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                fill='none'
+                points='29 13 14 29 25 29 23 39 38 23 27 23'
+              />
+            </svg>
+          </div>
+          <h6 className='mb-2 font-semibold leading-5 dark:text-gray-400'>
+            ReactJS
+          </h6>
+        </div>
+        <div className='text-center'>
+          <div className='flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-24 sm:h-24'>
+            <svg
+              className='w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16'
+              stroke='currentColor'
+              viewBox='0 0 52 52'
+            >
+              <polygon
+                strokeWidth='3'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                fill='none'
+                points='29 13 14 29 25 29 23 39 38 23 27 23'
+              />
+            </svg>
+          </div>
+          <h6 className='mb-2 font-semibold leading-5 dark:text-gray-400'>
+            TypeScript
+          </h6>
+        </div>
+        <div className='text-center'>
+          <div className='flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-24 sm:h-24'>
+            <svg
+              className='w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16'
+              stroke='currentColor'
+              viewBox='0 0 52 52'
+            >
+              <polygon
+                strokeWidth='3'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                fill='none'
+                points='29 13 14 29 25 29 23 39 38 23 27 23'
+              />
+            </svg>
+          </div>
+          <h6 className='mb-2 font-semibold leading-5 dark:text-gray-400'>
+            Node/Express
+          </h6>
+        </div>
+        <div className='text-center'>
+          <div className='flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-24 sm:h-24'>
+            <svg
+              className='w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16'
+              stroke='currentColor'
+              viewBox='0 0 52 52'
+            >
+              <polygon
+                strokeWidth='3'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                fill='none'
+                points='29 13 14 29 25 29 23 39 38 23 27 23'
+              />
+            </svg>
+          </div>
+          <h6 className='mb-2 font-semibold leading-5 dark:text-gray-400'>
+            Solidity
+          </h6>
+        </div>
+        <div className='text-center'>
+          <div className='flex items-center justify-center w-16 h-16 mx-auto mb-4 rounded-full bg-indigo-50 sm:w-24 sm:h-24'>
+            <svg
+              className='w-12 h-12 text-deep-purple-accent-400 sm:w-16 sm:h-16'
+              stroke='currentColor'
+              viewBox='0 0 52 52'
+            >
+              <polygon
+                strokeWidth='3'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                fill='none'
+                points='29 13 14 29 25 29 23 39 38 23 27 23'
+              />
+            </svg>
+          </div>
+          <h6 className='mb-2 font-semibold leading-5 dark:text-gray-400'>
+            UI Design
+          </h6>
+        </div>
+      </div>
+      <h2 className='prose text-gray-600 dark:text-gray-400 mb-6'>
+        Did I missed your favorite Stack? Worry not. I am always open to learn
+        new technologies if(tech !== 'PHP')
+      </h2>
+    </div>
+  );
+};
