1d0a4b984f789fca9eba8a7618055eeea0698d09
koushith <koushith97@gmail.com>
2022-12-09 16:54:13 +0530

    updates domain and other contents

--- components/Footer.js
+++ components/Footer.js
@@ -1,10 +1,10 @@
-import Link from 'next/link';
+import Link from "next/link";
 
 const ExternalLink = ({ href, children }) => (
   <a
-    className='text-gray-500 hover:text-gray-600 transition'
-    target='_blank'
-    rel='noopener noreferrer'
+    className="text-gray-500 hover:text-gray-600 transition"
+    target="_blank"
+    rel="noopener noreferrer"
     href={href}
   >
     {children}
@@ -14,64 +14,64 @@ const ExternalLink = ({ href, children }) => (
 export default function Footer() {
   return (
     <>
-      <hr className='w-full border-1 border-gray-200 dark:border-gray-800 mb-8' />
-      <footer class=' pt-10 sm:mt-10 pt-10'>
-        <div class='max-w-6xl m-auto text-gray-800 flex flex-wrap justify-center'>
-          <div class='p-5 w-1/2 sm:w-4/12 md:w-3/12'>
-            <div class='text-xs uppercase text-gray-400  hover:text-gray-900  font-medium mb-6'>
+      <hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
+      <footer class=" pt-10 sm:mt-10 pt-10">
+        <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-center">
+          <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="text-xs uppercase text-gray-400  hover:text-gray-900  font-medium mb-6">
               Main Links
             </div>
-            <div className='flex flex-col space-y-4'>
-              <Link href='/'>
-                <a className='text-gray-500  hover:text-gray-600 transition'>
+            <div className="flex flex-col space-y-4">
+              <Link href="/">
+                <a className="text-gray-500  hover:text-gray-600 transition">
                   Home
                 </a>
               </Link>
-              <Link href='/about'>
-                <a className='text-gray-500 hover:text-gray-600 transition'>
+              <Link href="/about">
+                <a className="text-gray-500 hover:text-gray-600 transition">
                   About
                 </a>
               </Link>
-              <Link href='/newsletter'>
-                <a className='text-gray-500 hover:text-gray-600 transition'>
+              <Link href="/newsletter">
+                <a className="text-gray-500 hover:text-gray-600 transition">
                   Newsletter
                 </a>
               </Link>
             </div>
           </div>
 
-          <div class='p-5 w-1/2 sm:w-4/12 md:w-3/12'>
-            <div class='text-xs uppercase text-gray-400 font-medium mb-6'>
+          <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
               Proof of Work
             </div>
-            <div className='flex flex-col space-y-4'>
-              <ExternalLink href='https://github.com/koushith'>
+            <div className="flex flex-col space-y-4">
+              <ExternalLink href="https://github.com/koushith">
                 GitHub
               </ExternalLink>
-              <ExternalLink href='https://twitter.com/KoushithAmin'>
+              <ExternalLink href="https://twitter.com/KoushithAmin">
                 Twitter
               </ExternalLink>
 
-              <ExternalLink href='https://dribbble.com/Koushith/'>
+              <ExternalLink href="https://dribbble.com/Koushith/">
                 Dribbble
               </ExternalLink>
             </div>
           </div>
 
-          <div class='p-5 w-1/2 sm:w-4/12 md:w-3/12'>
-            <div class='text-xs uppercase text-gray-400 font-medium mb-6'>
+          <div class="p-5 w-1/2 sm:w-4/12 md:w-3/12">
+            <div class="text-xs uppercase text-gray-400 font-medium mb-6">
               Social
             </div>
-            <div className='flex flex-col space-y-4'>
-              <ExternalLink href='https://www.linkedin.com/in/koushith-b-r-663787146/'>
+            <div className="flex flex-col space-y-4">
+              <ExternalLink href="https://www.linkedin.com/in/koushith-b-r-663787146/">
                 LinkedIn
               </ExternalLink>
-              <ExternalLink href='https://www.youtube.com/channel/UC2jU6KNhrAPJ4LIWDuiagnA'>
+              <ExternalLink href="https://www.youtube.com/channel/UC2jU6KNhrAPJ4LIWDuiagnA">
                 YouTube
               </ExternalLink>
 
-              <Link href='/snippets'>
-                <a className='text-gray-500 hover:text-gray-600 transition'>
+              <Link href="/snippets">
+                <a className="text-gray-500 hover:text-gray-600 transition">
                   Snippets
                 </a>
               </Link>
@@ -79,31 +79,32 @@ export default function Footer() {
           </div>
         </div>
 
-        <div class='pt-2'>
+        <div class="pt-2">
           <div
-            class='flex pb-5 px-3 m-auto pt-5 
+            class="flex pb-5 px-3 m-auto pt-5 
             border-t border-gray-500 text-gray-400 text-sm 
-            flex-col md:flex-row max-w-6xl'
+            flex-col md:flex-row max-w-6xl"
           >
-            <div class='mt-2'>
+            <div class="mt-2">
               © {new Date().getFullYear()}. Built with Next.js and TailwindCSS.
+              Portfolio inspired by Lee Robinson.
             </div>
 
-            <div class='md:flex-auto md:flex-row-reverse mt-2 flex-row flex'>
-              <a href='#' class='w-6 mx-1'>
-                <i class='uil uil-facebook-f'></i>
+            <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+              <a href="#" class="w-6 mx-1">
+                <i class="uil uil-facebook-f"></i>
               </a>
-              <a href='#' class='w-6 mx-1'>
-                <i class='uil uil-twitter-alt'></i>
+              <a href="#" class="w-6 mx-1">
+                <i class="uil uil-twitter-alt"></i>
               </a>
-              <a href='#' class='w-6 mx-1'>
-                <i class='uil uil-youtube'></i>
+              <a href="#" class="w-6 mx-1">
+                <i class="uil uil-youtube"></i>
               </a>
-              <a href='#' class='w-6 mx-1'>
-                <i class='uil uil-linkedin'></i>
+              <a href="#" class="w-6 mx-1">
+                <i class="uil uil-linkedin"></i>
               </a>
-              <a href='#' class='w-6 mx-1'>
-                <i class='uil uil-instagram'></i>
+              <a href="#" class="w-6 mx-1">
+                <i class="uil uil-instagram"></i>
               </a>
             </div>
           </div>
--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,22 @@
-import Link from 'next/link';
+import Link from "next/link";
 
-import Timeline from '../components/Timeline';
-import Container from '../components/Container';
-import Layout from '../components/Layout';
+import Timeline from "../components/Timeline";
+import Container from "../components/Container";
+import Layout from "../components/Layout";
 // import BlogPost from '../components/BlogPost';
 // import Subscribe from '../components/Subscribe';
-import ProjectCard from '../components/ProjectCard';
+import ProjectCard from "../components/ProjectCard";
 // import { Skills } from '@components/Skills';
 
 export default function Home() {
   return (
-    <div className='flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 '>
-      <h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white '>
+    <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 ">
+      <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white ">
         Hey, I’m Koushith Amin
       </h1>
-      <h2 className='prose text-gray-600 dark:text-gray-400 mb-6'>
-        I’m a Frontend Engineer and Visual Designer, living in Bengaluru. I'm currently building next generation DApps
-        at Consenso Labs.
+      <h2 className="prose text-gray-600 dark:text-gray-400 mb-6">
+        I’m a Frontend Engineer and Visual Designer, living in Bengaluru. I'm
+        currently building next generation DApps at Consenso Labs.
       </h2>
       {/* <div className='flex space-x-4'>
         <button className='flex items-center justify-center px-4 py-2  bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-100 rounded  '>
@@ -29,48 +29,76 @@ export default function Home() {
 
       {/* <Skills /> */}
 
-      <h3 className='font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white'>Projects</h3>
+      <h3 className="font-bold text-2xl md:text-4xl tracking-tight mb-4 mt-8 text-black dark:text-white">
+        Projects
+      </h3>
+
       <ProjectCard
-        title='Portex'
-        description='Secure crypto portfolio management and exchange application. Portex was on of the top 20 Projects for Celebrity Judging at the ETHDenver hackathon 2021 🏆'
-        href='https://github.com/koshikraj/portex'
-        icon='nextjs'
+        title="Ben Wallet"
+        description="Secure crypto portfolio management and exchange application. Portex was on of the top 20 Projects for Celebrity Judging at the ETHDenver hackathon 2021 🏆"
+        href="https://github.com/koshikraj/portex"
+        icon="nextjs"
       />
       <ProjectCard
-        title='SignChain'
-        description='Digital signing platform for legal documents. Signchain has bagged 2 sponsor prizes during the EthOnline hackathon 🎉 🎊.'
-        href='https://github.com/signchain/core'
-        icon='nextjs'
+        title="Safient Guardians"
+        description="Secure crypto portfolio management and exchange application. Portex was on of the top 20 Projects for Celebrity Judging at the ETHDenver hackathon 2021 🏆"
+        href="https://github.com/koshikraj/portex"
+        icon="nextjs"
       />
       <ProjectCard
-        title='Hack On'
-        description='The easiest way to add comments or reviews to your static site. Built using MERN Stack.'
-        href='https://hack-on-dev.herokuapp.com/'
-        icon='nextjs'
+        title="Safient Wallet"
+        description="Non-custodial recoverable and inheritable wallets by claiming them at the right time with ease and security. built during EthDenver Hackathon and won 3 bounties 🏆"
+        href="https://dorahacks.io/buidl/2222"
+        icon="nextjs"
       />
       <ProjectCard
-        title='Periple'
-        description='Periple- Online Dairy App -UI/UX Case Study. it was a part of 7days of Figma challenge created while learning UI Design'
-        href='https://koushith.netlify.app/blog05.html'
-        icon='nextjs'
+        title="Safient Protocol"
+        description="At Safient we strive to make digital self-custody safe and convenient and help preserve your digital legacy."
+        href="https://dorahacks.io/buidl/568"
+        icon="nextjs"
       />
       <ProjectCard
-        title='Medicare'
-        description='Medicare- Clinic Queue Management App. My first ever UI Design. covered an end to end Design Flow'
-        href='https://koushith.netlify.app/blog01.html'
-        icon='nextjs'
+        title="Ketch"
+        description="Fully functional ECommerce store developed using MERN Stack. fully inspired by Myntra Design"
+        href="https://github.com/Koushith/Ketch-Store"
+        icon="nextjs"
       />
       <ProjectCard
-        title='FEM Design System'
-        description='Building my own Component Library to Bootstrap personal projects. made use of React, Styled-Components and much more. inspired by IBM Plex.'
-        href='https://github.com/koushith/'
-        icon='nextjs'
+        title="Portex"
+        description="Secure crypto portfolio management and exchange application. Portex was on of the top 20 Projects for Celebrity Judging at the ETHDenver hackathon 2021 🏆"
+        href="https://github.com/koshikraj/portex"
+        icon="nextjs"
       />
       <ProjectCard
-        title='Documentation Series'
-        description='Documenting my Learning process along the way. This helps me to track my progress over time. '
-        href='https://www.youtube.com/channel/UC2jU6KNhrAPJ4LIWDuiagnA'
-        icon='nextjs'
+        title="SignChain"
+        description="Digital signing platform for legal documents. Signchain has bagged 2 sponsor prizes during the EthOnline hackathon 🎉 🎊."
+        href="https://github.com/signchain/core"
+        icon="nextjs"
+      />
+
+      <ProjectCard
+        title="Periple"
+        description="Periple- Online Dairy App -UI/UX Case Study. it was a part of 7days of Figma challenge created while learning UI Design"
+        href="https://koushith.netlify.app/blog05.html"
+        icon="nextjs"
+      />
+      <ProjectCard
+        title="Medicare"
+        description="Medicare- Clinic Queue Management App. My first ever UI Design. covered an end to end Design Flow"
+        href="https://koushith.netlify.app/blog01.html"
+        icon="nextjs"
+      />
+      <ProjectCard
+        title="FEM Design System"
+        description="Building my own Component Library to Bootstrap personal projects. made use of React, Styled-Components and much more. inspired by IBM Plex."
+        href="https://github.com/Koushith/Surprise"
+        icon="nextjs"
+      />
+      <ProjectCard
+        title="Documentation Series"
+        description="Documenting my Learning process along the way. This helps me to track my progress over time. "
+        href="https://www.youtube.com/channel/UC2jU6KNhrAPJ4LIWDuiagnA"
+        icon="nextjs"
       />
       <Timeline />
     </div>
