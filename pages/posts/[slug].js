cca4f391a0e391866f867e2572ec43c8d34647e9
koushith <koushith97@gmail.com>
2022-12-10 01:48:59 +0530

    updated new ghost blog integration

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,17 +1,17 @@
 // }
 
-import Link from 'next/link';
-import { useRouter } from 'next/router';
-import { useEffect, useState } from 'react';
-import Image from 'next/image';
-import BlogLayout from '../../components/BlogLayout';
-const CONTENT_API_KEY = '47225b98eaa1d61ea1463d34c0';
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import BlogLayout from "../../components/BlogLayout";
+const CONTENT_API_KEY = "f1801865dd6661863fa1d51f7f";
 async function getPost(slug) {
   const res = await fetch(
-    `https://koushith-portfolio-blog.herokuapp.com/ghost/api/v3/content/posts/slug/${slug}?key=${CONTENT_API_KEY}&fields=title,slug,html,feature_image,published_at,reading_time`
+    `https://ghost-production-824b.up.railway.app/ghost/api/v3/content/posts/slug/${slug}?key=${CONTENT_API_KEY}&fields=title,slug,html,feature_image,published_at,reading_time`
   ).then((res) => res.json());
 
-  console.log(res, 'res');
+  console.log(res, "res");
   const posts = res.posts;
 
   return posts[0];
@@ -19,7 +19,7 @@ async function getPost(slug) {
 
 // Ghost CMS Request
 export const getStaticProps = async ({ params }) => {
-  console.log('PARAMNS', params);
+  console.log("PARAMNS", params);
   const post = await getPost(params.slug);
   return {
     props: { post },
@@ -57,48 +57,48 @@ const Post = (props) => {
       this.page.identifier = post.slug;
     };
 
-    const script = document.createElement('script');
-    s.src = 'https://koushith-blog.disqus.com/embed.js';
-    script.setAttribute('data-timestamp', Date.now().toString());
+    const script = document.createElement("script");
+    s.src = "https://koushith-blog.disqus.com/embed.js";
+    script.setAttribute("data-timestamp", Date.now().toString());
 
     document.body.appendChild(script);
   }
 
   return (
     <>
-      <article className='flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full'>
-        <h1 className='font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white'>
+      <article className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16 w-full">
+        <h1 className="font-bold text-3xl md:text-5xl tracking-tight mb-4 text-black dark:text-white">
           {post.title}
         </h1>
-        <div className='flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2'>
-          <div className='flex items-center'>
+        <div className="flex flex-col md:flex-row justify-between items-start md:items-center w-full mt-2">
+          <div className="flex items-center">
             <Image
-              alt='Koushith Amin'
+              alt="Koushith Amin"
               height={24}
               width={24}
-              src='/avatar.jpg'
-              className='rounded-full'
+              src="/avatar.jpg"
+              className="rounded-full"
             />
-            <p className='text-sm text-gray-700 dark:text-gray-300 ml-2'>
+            <p className="text-sm text-gray-700 dark:text-gray-300 ml-2">
               {new Date(post.published_at).toDateString()}
             </p>
           </div>
-          <p className='text-sm text-gray-500 min-w-32 mt-2 md:mt-0'>
+          <p className="text-sm text-gray-500 min-w-32 mt-2 md:mt-0">
             {post.reading_time} min read
           </p>
         </div>
-        <img src={post.feature_image} className='my-10 ' />
+        <img src={post.feature_image} className="my-10 " />
 
         <div
-          className='prose dark:prose-dark max-w-none w-full'
+          className="prose dark:prose-dark max-w-none w-full"
           dangerouslySetInnerHTML={{ __html: post.html }}
         ></div>
-        <div className='mt-8'>{/* <Subscribe /> */}</div>
-        <div className='text-sm text-gray-700 dark:text-gray-300'>
+        <div className="mt-8">{/* <Subscribe /> */}</div>
+        <div className="text-sm text-gray-700 dark:text-gray-300">
           {enableLoadComments && <p onClick={loadComments}>Load Comments</p>}
           <div
-            style={{ width: '100% ', margin: '20px 0' }}
-            id='disqus_thread'
+            style={{ width: "100% ", margin: "20px 0" }}
+            id="disqus_thread"
           ></div>
         </div>
       </article>
